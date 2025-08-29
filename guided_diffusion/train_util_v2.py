8e5d328cf9018979884a873015b077c92ce268d2
Godwin-T <godwinolagunju0@gmail.com>
2025-08-29 13:23:28 +0100

    add fixes

--- guided_diffusion/train_util_v2.py
+++ guided_diffusion/train_util_v2.py
@@ -107,7 +107,7 @@ class TrainLoop:
             ]
         else:
             self.ema_params = [
-                copy.deepcopy(self.model.parameters())
+                copy.deepcopy(self.mp_trainer.master_params)
                 for _ in range(len(self.ema_rate))
             ]
 
@@ -171,7 +171,7 @@ class TrainLoop:
         dist_util.sync_params(self.model.parameters())
 
     def _load_ema_parameters(self, rate):
-        ema_params = copy.deepcopy(self.model.parameters())
+        ema_params = copy.deepcopy(self.mp_trainer.master_params)
 
         main_checkpoint = find_resume_checkpoint() or self.resume_checkpoint
         ema_checkpoint = find_ema_checkpoint(main_checkpoint, self.resume_step, rate)
@@ -181,7 +181,7 @@ class TrainLoop:
                 state_dict = dist_util.load_state_dict(
                     ema_checkpoint, map_location=dist_util.dev()
                 )
-                ema_params = self.model.state_dict()
+                ema_params = self.mp_trainer.state_dict_to_master_params(state_dict)
 
         dist_util.sync_params(ema_params)
         return ema_params
@@ -283,14 +283,14 @@ class TrainLoop:
 
     def run_step(self, batch, cond):
         self.forward_backward(batch, cond)
-        took_step = self.model.optimize(self.opt)
+        took_step = self.mp_trainer.optimize(self.opt)
         if took_step:
             self._update_ema()
         self._anneal_lr()
         self.log_step()
 
     def forward_backward(self, batch, cond):
-        self.model.zero_grad()
+        self.mp_trainer.zero_grad()
         print("*"*50)
         print(self.current_device)
         print("*"*50)
@@ -334,7 +334,7 @@ class TrainLoop:
 
     def _update_ema(self):
         for rate, params in zip(self.ema_rate, self.ema_params):
-            update_ema(params, self.model.parameters(), rate=rate)
+            update_ema(params, self.mp_trainer.master_params, rate=rate)
 
     def _anneal_lr(self):
         if not self.lr_anneal_steps:
