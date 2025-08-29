e7b7413c7bc6d47dbd6fa61d375bcff5e69abf3a
Godwin-T <godwinolagunju0@gmail.com>
2025-08-29 14:35:54 +0100

    add fixes

--- guided_diffusion/train_util_v2.py
+++ guided_diffusion/train_util_v2.py
@@ -540,6 +540,8 @@ class TrainLoop:
                 bucket_cap_mb=128,
                 find_unused_parameters=False,
             )
+            if self.use_fp16:
+                self.ddp_model.convert_to_fp16() 
             logger.log(f"Using GPU {self.rank} with DDP, {th.cuda.device_count()} GPUs available")
         else:
             self.use_ddp = False
